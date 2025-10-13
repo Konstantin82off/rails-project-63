33ca963f5a935dc9d08f31c30e0f60f42fe8a44a
Konstantin82off <k.udodov@internet.ru>
2025-10-13 19:47:57 +0300

    Add documentation comment for HexletCode::Tag

--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -1,33 +1,41 @@
 # frozen_string_literal: true
 
-module HexletCode
+module HexletCode # :nodoc:
+  # Helpers to build HTML tags.
   module Tag
     VOID_TAGS = %w[area base br col embed hr img input link meta param source track wbr].freeze
 
-    def self.build(name, attrs = {})
+    def self.build(name, attrs = {}, &block)
       tag_name = name.to_s
       attributes = build_attributes(attrs)
 
       if VOID_TAGS.include?(tag_name)
         "<#{tag_name}#{attributes}>"
       else
-        content = block_given? ? yield : ''
+        content = block_given? ? block.call : ""
         "<#{tag_name}#{attributes}>#{content}</#{tag_name}>"
       end
     end
 
     def self.build_attributes(attrs)
-      return '' if attrs.nil? || attrs.empty?
+      return "" if attrs.nil? || attrs.empty?
 
-      parts = []
-      attrs.each do |key, value|
-        next if value.nil?
-        parts << "#{key}="#{value}""
-      end
-
-      parts.empty? ? '' : " #{parts.join(' ')}"
+      parts = attributes_parts(attrs)
+      parts.empty? ? "" : " #{parts.join(" ")}"
     end
 
-    private_class_method :build_attributes
+    def self.attributes_parts(attrs)
+      attrs.each_with_object([]) do |(key, value), acc|
+        next if skip_attribute?(value)
+
+        acc << (value == true ? key.to_s : %(#{key}="#{value}"))
+      end
+    end
+
+    def self.skip_attribute?(value)
+      value.nil? || value == false
+    end
+
+    private_class_method :build_attributes, :attributes_parts, :skip_attribute?
   end
 end
