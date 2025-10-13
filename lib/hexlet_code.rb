bd664c8939fc50a37cd4eac34b3501c7319d7f14
Konstantin82off <k.udodov@internet.ru>
2025-10-13 09:41:20 +0300

    fix: fixed the We need to fix the lib/hexlet_code.rb file.

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,9 +1,9 @@
-\# frozen_string_literal: true
+# frozen_string_literal: true
 
-require_relative 'hexlet_code/version'
+require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
 
-  autoload :Tag, 'hexlet_code/tag'
+  autoload :Tag, "hexlet_code/tag"
 end
