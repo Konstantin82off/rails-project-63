3b92bbb5d4a48c2240cd4c6191b5b165779306f7
Konstantin82off <k.udodov@internet.ru>
2025-11-10 12:04:19 +0300

    Fix textarea rendering in RSpec tests
    Changes:
    - In FormBuilder:
      * Refactored input method to use build_field helper
      * Added clean_non_textarea_dims! to remove rows/cols from non-textarea inputs
      * Improved type extraction logic in extract_type
      * Added extract_value method for better value handling
    - In TextareaInput:
      * Updated render method to properly merge default and user-provided options
      * Ensured name attribute is always a string
      * Made sure value is converted to string in block
    
    Fixes RSpec test failure where textarea was not found due to incorrect HTML generation.

--- lib/hexlet_code/form_builder.rb
+++ lib/hexlet_code/form_builder.rb
@@ -14,31 +14,58 @@ module HexletCode
       @submit_value = nil
     end
 
-    def input(name, **options)
-      type = extract_type(options)
-      value = @entity.public_send(name)
-
-      @fields << {
-        type: type,
-        name: name,
-        value: value,
-        options: options
-      }
+    def input(name, options = {})
+      @fields << build_field(name, options)
+      self
     end
 
     def submit(value = 'Save')
       @submit_value = value
+      self
     end
 
     private
 
-    def extract_type(options)
-      as = options[:as]&.to_sym
-      case as
-      when :textarea then :textarea
-      when nil then :text
-      else as
+    def build_field(name, options)
+      opts  = options.dup
+      type  = extract_type(opts.delete(:as))
+      value = extract_value(name, opts)
+      clean_non_textarea_dims!(opts, type)
+
+      { type: type, name: name, value: value, options: opts }
+    end
+
+    def clean_non_textarea_dims!(opts, type)
+      return if type == :textarea
+
+      opts.delete(:rows)
+      opts.delete(:cols)
+    end
+
+    def extract_type(as)
+      return :text if as.nil?
+
+      sym = as.to_sym
+      return :textarea if %i[text textarea].include?(sym)
+
+      input_class_name = "#{camelize(sym)}Input"
+      if HexletCode::Inputs.const_defined?(input_class_name, false)
+        sym
+      else
+        :text
       end
+    end
+
+    def extract_value(name, options)
+      return options.delete(:value) if options.key?(:value)
+
+      raise NoMethodError, "Unknown attribute #{name} for #{@entity.class}" unless @entity.respond_to?(name)
+
+      @entity.public_send(name)
+    end
+
+    def camelize(sym)
+      sym.to_s.split('_').map!(&:capitalize).join
     end
   end
 end
